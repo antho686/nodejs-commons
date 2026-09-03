8b39896ec5700618dbf03d406a40c83e1c31b5c0
antho686 <pamorel@precisiodata.com>
2026-09-03 16:52:31 -0400

    fix esm exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-export {
-  createDeterministicObjectHash,
-  deterministicObjectHash,
-  HexLength,
-} from './hash/deterministicObjectHash';
-export type {
-  DeterministicObjectHasher,
-  DeterministicObjectHashOptions,
-  FlatJsonObject,
-  FlatJsonValue,
-} from './hash/deterministicObjectHash';
+export * from './hash'
